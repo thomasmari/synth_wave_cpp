b01f0e08865916d156604200caa1ea5703afaaad
DidiCi <dilettaciardo@yahoo.it>
2026-02-18 10:23:32 +0100

    modif_keyboard

--- Keyboard.cpp
+++ Keyboard.cpp
@@ -2,13 +2,27 @@
 
 #include "ofMain.h"
 
-class Keyboard : public ofBaseApp {
+void Keyboard::setup(){
+    keyWidth = ofGetWidth() / numWhiteKeys;
+    keyHeight = ofGetHeight() * 0.4;
 
-	public:
-		// Method
-		void setup();
-		void draw();
-		float get_frequency (int key);
-		// Attributes
-		vector <float> keyList;
-};
+    whitePressed.assign(numWhiteKeys, false);
+    blackPressed.assign(numWhiteKeys, false);
+
+
+}
+
+void draw(){
+
+    for (unsigned i=0; i < whiteMap.size(); i++){
+        if whitePressed[i]{
+
+        }
+    }
+
+}
+
+float get_frequency (int key);
+void keyPressed(int key);
+void keyReleased(int key);
+
--- Keyboard.h
+++ Keyboard.h
@@ -2,15 +2,25 @@
 
 #include "ofMain.h"
 
-class Keyboard : public ofBaseApp {
+class Keyboard{
 
-	public:
-		// Method
-		void setup();
-		void draw();
-		float get_frequency (int key);
-		void keyPressed(int key);
-		void keyReleased(int key);
-		// Attributes
-		vector <float> keyList;
+        public:
+                // Method
+                void setup();
+                void draw();
+                float get_frequency (int key);
+                void keyPressed(int key);
+                void keyReleased(int key);
+
+                // Attributes
+                vector <float> keyList;
+                vector<bool> whitePressed;
+        vector<bool> blackPressed;
+                vector<int> whiteMap = {'q','s','d','f','g','h','j','k','l'};
+                vector<int> blackMap = {'z','e','t','y','u','o','p'};
+
+                int numWhiteKeys = 10;
+                float keyWidth;
+                float keyHeight;
+
 };
